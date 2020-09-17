549e69c40ef81b21563dfd860fea3ad5f40eb4b0
Felipe-Borba <felipe.borba0109@gmail.com>
2020-09-17 10:36:49 -0300

    atualização ula
    segundo a aula do dia 17 mas nao sei se ta funcionando corretamente

--- ULA/ULA.vhd
+++ ULA/ULA.vhd
@@ -6,21 +6,39 @@ entity ULA is
 	generic (n: natural := 4);
 	port (A: in std_logic_vector(n-1 downto 0);
 			B: in std_logic_vector(n-1 downto 0);
+			Cin: in std_logic;
 			sel: in std_logic_vector(2 downto 0);
+			zero: out std_logic;
+			overflow: out std_logic;
+			carry_out: out std_logic;
+			negativo: out std_logic;
 			saida: out std_logic_vector(n-1 downto 0)
 			);
 end ULA;
 
 architecture comportamento of ULA is
 signal K1: std_logic_vector(n-1 downto 0);
+signal K2: std_logic_vector(n-1 downto 0) := (others => '0');
+signal K3: std_logic_vector(n downto 0);
+signal K4: std_logic_vector(n downto 0);
+
 begin
 	K1 <= (0 => '1', others => '0') when A < B else 
 			(others => '0');
 			
-	saida <= (A + B) when sel = "000" else
-				(A - B) when sel = "001" else
-				(A or B) when sel = "010" else
-				(A and B) when sel = "011" else
-				NOT(A) when sel = "100" else
-				K1;
+	carry_out <= K4(n) or K3(n);
+	overflow <= (A(n-1) and B(n-1)) xor (A(n-2) and B(n-2));
+	negativo <= K4(n-1) or K3(n-1);
+	zero <= '0' when (A-B) = K2;
+	K3 <= ('0' & A) + ('0' & B)+ Cin;
+	K4 <= ('0' & A) - ('0' & B)+ Cin;
+	
+	saida <= (K3(n-1 downto 0))	when sel = "000" else
+				(K4(n-1 downto 0)) 	when sel = "001" else
+				(A or B) 				when sel = "010" else
+				(A and B) 				when sel = "011" else
+				(NOT(A)) 				when sel = "100" else
+			   (K1) 						when sel = "101" else
+				(A xnor B) 				when sel = "110" else
+				(A xor B);
 end comportamento;
